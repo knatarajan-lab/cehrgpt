e5acd759958ae52e079c86af6859bbf46aa3b739
Chao Pang <ChaoPang229@gmail.com>
2024-11-02 12:53:38 -0400

    fixed CehrGPTArguments

--- src/cehrgpt/runners/hf_gpt_runner_argument_dataclass.py
+++ src/cehrgpt/runners/hf_gpt_runner_argument_dataclass.py
@@ -6,27 +6,21 @@ from typing import Optional
 class CehrGPTArguments:
     """Arguments pertaining to what data we are going to input our model for training and eval."""
 
-    expand_tokenizer: Optional[bool] = (
-        dataclasses.field(
-            default=False,
-            metadata={
-                "help": "A flag to indicate whether we want to expand the tokenizer for fine-tuning."
-            },
-        ),
+    expand_tokenizer: Optional[bool] = dataclasses.field(
+        default=False,
+        metadata={
+            "help": "A flag to indicate whether we want to expand the tokenizer for fine-tuning."
+        },
     )
-    hyperparameter_tuning_percentage: Optional[float] = (
-        dataclasses.field(
-            default=0.1,
-            metadata={
-                "help": "The percentage of the train/val will be use for hyperparameter tuning."
-            },
-        ),
+    hyperparameter_tuning_percentage: Optional[float] = dataclasses.field(
+        default=0.1,
+        metadata={
+            "help": "The percentage of the train/val will be use for hyperparameter tuning."
+        },
     )
-    n_trials: Optional[int] = (
-        dataclasses.field(
-            default=10,
-            metadata={
-                "help": "The number of trails will be use for hyperparameter tuning."
-            },
-        ),
+    n_trials: Optional[int] = dataclasses.field(
+        default=10,
+        metadata={
+            "help": "The number of trails will be use for hyperparameter tuning."
+        },
     )
