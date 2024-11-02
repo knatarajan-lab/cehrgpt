3599f068583679056c0a429b05793d70b82d7379
Chao Pang <ChaoPang229@gmail.com>
2024-11-02 16:53:14 -0400

    removed resume_from_checkpoint=True, from hyperparameter_search

--- src/cehrgpt/runners/hf_cehrgpt_finetune_runner.py
+++ src/cehrgpt/runners/hf_cehrgpt_finetune_runner.py
@@ -433,7 +433,6 @@ def main():
                 OptunaMetricCallback(),
             ],
             args=training_args,
-            resume_from_checkpoint=True,
         )
         # Perform hyperparameter search
         hp_space_partial = partial(
