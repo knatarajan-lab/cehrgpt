9f2cd0334d9300585dab60b10fede6f497cb9789
Chao Pang <ChaoPang229@gmail.com>
2024-12-30 10:45:11 -0500

    fixed the NAN value loss bug when there is no value associated with the sample in the batch

--- src/cehrgpt/models/hf_cehrgpt.py
+++ src/cehrgpt/models/hf_cehrgpt.py
@@ -1226,15 +1226,13 @@ class CEHRGPT2LMHeadModel(CEHRGPTPreTrainedModel):
             shift_value_logits = value_logits[..., :-1, :].contiguous()
             shift_value_indicators = true_value_indicators[..., :-1].contiguous()
             shift_next_values = true_values[..., 1:].contiguous()
-            shift_next_values = torch.where(
-                shift_value_indicators, shift_next_values, -100
-            )
-            value_loss_fct = CrossEntropyLoss()
+            value_loss_fct = CrossEntropyLoss(reduce=False)
             token_value_loss = value_loss_fct(
                 shift_value_logits.view(-1, shift_value_logits.size(-1)),
                 shift_next_values.view(-1),
             )
-            loss += token_value_loss
+            token_value_loss *= shift_value_indicators.view(-1)
+            loss += token_value_loss.mean()
 
         if not return_dict:
             output = (lm_logits,) + transformer_outputs[1:]
