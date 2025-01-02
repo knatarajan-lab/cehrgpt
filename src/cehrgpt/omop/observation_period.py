17153321849768dc080a7d4255a99d90e2759af1
Chao Pang <ChaoPang229@gmail.com>
2025-01-02 14:38:58 -0500

    added measurement to observation_period

--- src/cehrgpt/omop/observation_period.py
+++ src/cehrgpt/omop/observation_period.py
@@ -1,24 +1,38 @@
+import os.path
+
 from cehrgpt.omop.omop_argparse import create_omop_argparse
 from cehrgpt.omop.omop_table_builder import OmopTableBuilder
-from cehrgpt.omop.queries.observation_period import OBSERVATION_PERIOD_QUERY
+from cehrgpt.omop.queries.observation_period import (
+    OBSERVATION_PERIOD_QUERY,
+    OBSERVATION_PERIOD_WITH_MEASUREMENT_QUERY,
+)
 
 OBSERVATION_PERIOD = "observation_period"
 
 
 def main(args):
+    include_measurement = os.path.exists(os.path.join(args.input_folder, "measurement"))
+    dependency_list = [
+        "person",
+        "visit_occurrence",
+        "condition_occurrence",
+        "procedure_occurrence",
+        "drug_exposure",
+    ]
+    if include_measurement:
+        dependency_list.append("measurement")
+
     OmopTableBuilder.create_omop_query_builder(
         input_folder=args.input_folder,
         output_folder=args.output_folder,
         continue_job=args.continue_job,
         table_name=OBSERVATION_PERIOD,
-        query_template=OBSERVATION_PERIOD_QUERY,
-        dependency_list=[
-            "person",
-            "visit_occurrence",
-            "condition_occurrence",
-            "procedure_occurrence",
-            "drug_exposure",
-        ],
+        query_template=(
+            OBSERVATION_PERIOD_WITH_MEASUREMENT_QUERY
+            if include_measurement
+            else OBSERVATION_PERIOD_QUERY
+        ),
+        dependency_list=dependency_list,
     ).build()
 
 
