f269b3ec7073989d88e3dd3f4d8353b0041c0efd
Chao Pang <ChaoPang229@gmail.com>
2025-01-09 16:06:23 -0500

    fixed the import of tqdm

--- src/cehrgpt/tools/generate_pretrained_embeddings.py
+++ src/cehrgpt/tools/generate_pretrained_embeddings.py
@@ -1,18 +1,25 @@
 import argparse
-from sklearn.preprocessing import normalize
-import torch
-from transformers import AutoModel, AutoTokenizer
-import pandas as pd
-import tqdm
-from typing import List, Generator
-import numpy as np
 import os
 import pickle
-from cehrgpt.models.pretrained_embeddings import PRETRAINED_EMBEDDING_VECTOR_FILE_NAME, PRETRAINED_EMBEDDING_CONCEPT_FILE_NAME
+from typing import Generator, List
+
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.preprocessing import normalize
+from tqdm import tqdm
+from transformers import AutoModel, AutoTokenizer
+
+from cehrgpt.models.pretrained_embeddings import (
+    PRETRAINED_EMBEDDING_CONCEPT_FILE_NAME,
+    PRETRAINED_EMBEDDING_VECTOR_FILE_NAME,
+)
 
 
 def generate_embeddings_batch(texts, tokenizer, device, model):
-    input = tokenizer(texts, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    input = tokenizer(
+        texts, return_tensors="pt", padding=True, truncation=True, max_length=512
+    )
     input = {k: v.to(device) for k, v in input.items()}
 
     with torch.no_grad():
@@ -21,31 +28,39 @@ def generate_embeddings_batch(texts, tokenizer, device, model):
     return normalize(embeddings)
 
 
-def create_batches(texts: List[str], batch_size: int) -> Generator[List[str], None, None]:
+def create_batches(
+    texts: List[str], batch_size: int
+) -> Generator[List[str], None, None]:
     """Generate batches of texts."""
     for i in range(0, len(texts), batch_size):
-        yield texts[i:i + batch_size]
+        yield texts[i : i + batch_size]
 
 
 def main(args):
-    tokenizer = AutoTokenizer.from_pretrained("dunzhang/stella_en_1.5B_v5", trust_remote_code=True)
-    model = AutoModel.from_pretrained("dunzhang/stella_en_1.5B_v5", trust_remote_code=True).eval()
+    tokenizer = AutoTokenizer.from_pretrained(
+        "dunzhang/stella_en_1.5B_v5", trust_remote_code=True
+    )
+    model = AutoModel.from_pretrained(
+        "dunzhang/stella_en_1.5B_v5", trust_remote_code=True
+    ).eval()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
 
-    vocab = pd.read_json(args.vocab_json_file_path, typ='series').reset_index()
-    vocab.columns = ['concept_id', 'idx']
+    vocab = pd.read_json(args.vocab_json_file_path, typ="series").reset_index()
+    vocab.columns = ["concept_id", "idx"]
 
-    vocab.drop_duplicates(subset=['concept_id'], inplace=True)
+    vocab.drop_duplicates(subset=["concept_id"], inplace=True)
     vocab = vocab.astype(str)
 
     concept = pd.read_parquet(args.concept_parquet_file_path)
     concept = concept.astype(str)
 
-    vocab_with_name = vocab.merge(concept, how='inner', left_on='concept_id', right_on='concept_id')
+    vocab_with_name = vocab.merge(
+        concept, how="inner", left_on="concept_id", right_on="concept_id"
+    )
 
-    concept_names = vocab_with_name['concept_name'].to_list()
-    data_with_name = vocab_with_name[['concept_id', 'concept_name']]
+    concept_names = vocab_with_name["concept_name"].to_list()
+    data_with_name = vocab_with_name[["concept_id", "concept_name"]]
 
     total_batches = (len(concept_names) + args.batch_size - 1) // args.batch_size
 
@@ -54,21 +69,29 @@ def main(args):
     with tqdm(total=total_batches, desc="Processing batches") as pbar:
         for batch in create_batches(concept_names, args.batch_size):
             try:
-                batch_embeddings = generate_embeddings_batch(batch, tokenizer, device, model)
+                batch_embeddings = generate_embeddings_batch(
+                    batch, tokenizer, device, model
+                )
                 all_embeddings.extend(batch_embeddings)
             except Exception as e:
                 print(f"Error processing batch: {str(e)}")
 
-    concept_dict = data_with_name.to_dict('records')
+    concept_dict = data_with_name.to_dict("records")
 
-    np.save(os.path.join(args.output_folder_path, PRETRAINED_EMBEDDING_VECTOR_FILE_NAME), all_embeddings)
+    np.save(
+        os.path.join(args.output_folder_path, PRETRAINED_EMBEDDING_VECTOR_FILE_NAME),
+        all_embeddings,
+    )
 
-    with open(os.path.join(args.output_folder_path, PRETRAINED_EMBEDDING_CONCEPT_FILE_NAME), 'wb') as file:
+    with open(
+        os.path.join(args.output_folder_path, PRETRAINED_EMBEDDING_CONCEPT_FILE_NAME),
+        "wb",
+    ) as file:
         pickle.dump(concept_dict, file)
 
 
 def create_arg_parser():
-    parser = argparse.ArgumentParser(description='Create pretrained embeddings')
+    parser = argparse.ArgumentParser(description="Create pretrained embeddings")
     parser.add_argument(
         "--vocab_json_file_path",
         dest="vocab_json_file_path",
