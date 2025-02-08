ad525670f4c6df6633a62238253feeddd67fc5cb
Chao Pang <ChaoPang229@gmail.com>
2025-02-08 07:46:28 -0500

    fixed the unit test

--- tests/unit_tests/generation/clinical_statement_generator_test.py
+++ tests/unit_tests/generation/clinical_statement_generator_test.py
@@ -32,10 +32,10 @@ class TestClinicalStatementGenerator(unittest.TestCase):
     def test_get_indications(self):
         # Test to ensure caching and retrieval of indications
         indications = self.generator.get_indications(1)
-        self.assertEqual(indications, [2, 200, 201])
+        self.assertDictEqual(indications, {2: 2, 200: 2, 201: 2})
         # Check if repeated calls use the cache
         indications = self.generator.get_indications(1)
-        self.assertEqual(indications, [2, 200, 201])
+        self.assertDictEqual(indications, {2: 2, 200: 2, 201: 2})
 
     def test_is_allowed_condition(self):
         event_allowed = CehrGptEvent(
@@ -59,7 +59,7 @@ class TestClinicalStatementGenerator(unittest.TestCase):
 
     def test_generate_clinical_statement_with_data(self):
         # Testing generation of clinical statement with mock data
-        self.generator.get_indications = MagicMock(return_value=[200])
+        self.generator.get_indications = MagicMock(return_value={200: 200})
         clinical_statement = self.generator.generate_clinical_statement(
             concept_ids=[
                 "year:2021",
@@ -83,7 +83,7 @@ class TestClinicalStatementGenerator(unittest.TestCase):
         )
         self.assertIn("Race", clinical_statement)
         self.assertIn("Gender", clinical_statement)
-        self.assertIn("Diagnosis age", clinical_statement)
+        self.assertIn("Age", clinical_statement)
         self.assertIn("Condition", clinical_statement)
         self.assertIn("Drug", clinical_statement)
 
