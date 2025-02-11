71336739a5cd2bba7c70be522e0bf0a64157e930
Chao Pang <ChaoPang229@gmail.com>
2025-02-11 11:16:57 -0500

    set the default value for dev_model to False

--- src/cehrgpt/chat/config.py
+++ src/cehrgpt/chat/config.py
@@ -6,4 +6,4 @@ class Config:
     TOKENIZER_PATH = os.getenv("tokenizer_path")
     MODEL_PATH = os.getenv("model_path")
     USE_LLM_PARSER = bool(int(os.getenv("use_llm_parser", "0")))
-    DEV_MODE = bool(int(os.getenv("dev_model", "1")))
+    DEV_MODE = bool(int(os.getenv("dev_model", "0")))
