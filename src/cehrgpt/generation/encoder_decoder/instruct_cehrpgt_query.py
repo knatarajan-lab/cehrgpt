c4bd7d5c17daa44200e82de14b365b99dff0a159
Chao Pang <ChaoPang229@gmail.com>
2025-02-15 16:37:38 -0500

    Front end (#46)
    * print out use_grpo for verification
    
    * moved openai client inside of the function
    
    * added a hashing function in CehrGptEvent so we can avoid adding the same record
    
    * updated the cehrgpt narrative format
    
    * added a local flask app for testing
    
    * fixed the imports
    
    * fixed the model response in the flask app
    
    * added an option to print patient narrative in HTML
    
    * try rendering the patient history in javascript
    
    * updated the javascript logic
    
    * fixed the chatbox
    
    * added a download button
    
    * use claude to update the HTML
    
    * added the logic for handling inpatient and outpatient visits
    
    * try breaking the html file into multiple files
    
    * added a test patient to support local development
    
    * set the default value for dev_model to False
    
    * fixed the scrolling behavior
    
    * modified parse_question_to_cehrgpt_query to return a tuple of (query, n_patients)
    
    * added a debug statement
    
    * updated the front end
    
    * added celery and redis to run the model in the background
    
    * fixed the patient-stats endpoint so it returns stats associated with the task_id
    
    * added logic to keep track of the conversations
    
    * set window.task_id in task_results.html
    
    * updated the endpoint of patients-stats
    
    * try to capture exceptions from the endpoint
    
    * updated the model_utils to use spawning instead of forking
    
    * fixed the endpoint get_patient_stats
    
    * fixed the logic for retrieving the patient stats when the task is in PROGRESS
    
    * added user query to the result page regardless of the task state
    
    * removed the window.open
    
    * updated index.html so the welcome message always stays
    
    * added a spinner to indicate the model is thinking
    
    * updated result page to show the original user query
    
    * moved the logic for handling the empty message to the right place when the chat-form is submitted
    
    * added the query to the response when the task is in PENDING
    
    * update the progressbar every batch
    
    * comment out Patient Details
    
    * updated the react components to have more space between figures
    
    * added __hash__ to CehrGptVisit
    
    * added a check on whether a similar visit already exists in the previous visits by checking the hash value of the new visit
    
    * removed the download button from index.html
    
    * changed the welcome message css style
    
    * moved chatgpt_generation to generation/chatgpt
    
    * added a synthetic patient baseline generator to use chatgpt to generate patient timelines
    
    * updated the chatgpt_cehrgpt_narrative.py
    
    * updated cehrbert to 1.3.5
    
    * fixed the unit test for printing the narrative

--- src/cehrgpt/generation/encoder_decoder/instruct_cehrpgt_query.py
+++ src/cehrgpt/generation/encoder_decoder/instruct_cehrpgt_query.py
@@ -1,18 +1,21 @@
 import os
 from textwrap import dedent
-from typing import Optional
+from typing import Optional, Tuple
 
 from jinja2 import BaseLoader, Environment
 from openai import OpenAI
 from pydantic import BaseModel
+from transformers.utils import logging
 
 from cehrgpt.generation.cehrgpt_patient.clinical_statement_generator import (
     create_clinical_statement,
 )
 
+logger = logging.get_logger("transformers")
+
 MODEL = "gpt-4o-2024-08-06"
 TEMPLATE = """
-Instructions: Analyze the clinical statement provided and extract the following information: race, gender, diagnosis,
+Instructions: Analyze the clinical statement provided and extract the following information for synthetic data generation: number of patients, race, gender, diagnosis,
 age at diagnosis, and the medication prescribed. If any information is not available in the statement,
 leave the corresponding field blank.
 
@@ -37,6 +40,7 @@ Example Responses:
 Example 1:
 Statement: A white male was diagnosed with essential hypertension at the age of 50 and he was being treated with ACE inhibitors.
 Expected Answer:
+Number of patients: 1
 Race: White
 Gender: Male
 Age of diagnosis: 50
@@ -44,8 +48,9 @@ Diagnosis: Essential Hypertension
 Drug: ACE Inhibitors
 
 Example 2:
-Statement: A female was diagnosed with T2DM at the age of 40.
+Statement: 10 female patients who were diagnosed with T2DM at the age of 40.
 Expected Answer:
+Number of patients: 10
 Race:
 Gender: Female
 Age of diagnosis: 40
@@ -55,7 +60,6 @@ Drug:
 
 ENV = Environment(loader=BaseLoader(), autoescape=True)
 QUERY_TEMPLATE = ENV.from_string(TEMPLATE)
-client = OpenAI(api_key=os.environ.get("OPEN_AI_KEY"))
 
 
 class InstructCehrGptQueryTemplate(BaseModel):
@@ -64,9 +68,13 @@ class InstructCehrGptQueryTemplate(BaseModel):
     age_of_diagnosis: int
     diagnosis: str
     drug: str
+    num_of_patients: int
 
 
-def parse_question_to_cehrgpt_query(clinical_statement: str) -> Optional[str]:
+def parse_question_to_cehrgpt_query(
+    clinical_statement: str,
+) -> Optional[Tuple[str, int]]:
+    client = OpenAI(api_key=os.environ.get("OPEN_AI_KEY", None))
     prompt = QUERY_TEMPLATE.render(clinical_statement=clinical_statement)
     completion = client.beta.chat.completions.parse(
         model=MODEL,
@@ -77,17 +85,24 @@ def parse_question_to_cehrgpt_query(clinical_statement: str) -> Optional[str]:
         response_format=InstructCehrGptQueryTemplate,
     )
     parsed = completion.choices[0].message.parsed
+    logger.debug("ChatGPT response: %s", completion.choices[0].message)
     if parsed:
         race = parsed.race
         gender = parsed.gender
         age_of_diagnosis = parsed.age_of_diagnosis
         diagnosis = parsed.diagnosis
         drug = parsed.drug
+        num_of_patients = parsed.num_of_patients
         if not race and not gender and not diagnosis:
             return None
-        return create_clinical_statement(
+        # Default to one patient if LLM fails to identify this
+        if not num_of_patients:
+            num_of_patients = 1
+
+        clinical_statement = create_clinical_statement(
             gender=gender,
             race=race,
             age_condition_drug_tuples=[(age_of_diagnosis, diagnosis, drug)],
         )
+        return clinical_statement, num_of_patients
     return None
