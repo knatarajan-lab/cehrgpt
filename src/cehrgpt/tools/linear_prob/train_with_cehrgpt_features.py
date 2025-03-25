0fad1765f1bd3aac36605e4d57f2c52207886c81
Chao Pang <ChaoPang229@gmail.com>
2025-03-25 11:51:59 -0400

    added gender, race and age to the feature list

--- src/cehrgpt/tools/linear_prob/train_with_cehrgpt_features.py
+++ src/cehrgpt/tools/linear_prob/train_with_cehrgpt_features.py
@@ -1,8 +1,9 @@
 import argparse
 import functools
 import json
+import pickle
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 import lightgbm as lgb
 import numpy as np
@@ -11,6 +12,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import auc, precision_recall_curve, roc_auc_score
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 
 def lightgbm_objective(trial, *, train_data, dev_data, num_trees=None):
@@ -48,11 +50,24 @@ def lightgbm_objective(trial, *, train_data, dev_data, num_trees=None):
     return error
 
 
-def prepare_gbm_dataset(df: pd.DataFrame) -> Dict[str, Any]:
+def prepare_dataset(
+    df: pd.DataFrame, feature_processor: Dict[str, Union[StandardScaler, OneHotEncoder]]
+) -> Dict[str, Any]:
+    age_scaler = feature_processor["age_scaler"]
+    gender_encoder = feature_processor["gender_encoder"]
+    race_encoder = feature_processor["race_encoder"]
+    scaled_age = age_scaler.transform(df[["age_at_index"]].to_numpy())
+    one_hot_gender = gender_encoder.transform(df[["gender_concept_id"]].to_numpy())
+    one_hot_race = race_encoder.transform(df[["gender_concept_id"]].to_numpy())
+
+    features = np.stack(df["features"].apply(lambda x: np.array(x).flatten()))
+    concatenated_features = np.concatenate(
+        [scaled_age, one_hot_gender, one_hot_race, features], axis=1
+    )
     return {
         "subject_id": df["subject_id"].tolist(),
         "prediction_time": df["prediction_time"].tolist(),
-        "features": np.stack(df["features"].apply(lambda x: np.array(x).flatten())),
+        "features": concatenated_features,
         "boolean_value": df["boolean_value"].to_numpy(),
     }
 
@@ -60,6 +75,7 @@ def prepare_gbm_dataset(df: pd.DataFrame) -> Dict[str, Any]:
 def main(args):
     features_data_dir = Path(args.features_data_dir)
     output_dir = Path(args.output_dir)
+    feature_processor_path = output_dir / "feature_processor.pickle"
     logistic_dir = output_dir / "logistic"
     logistic_dir.mkdir(exist_ok=True, parents=True)
     gbm_dir = output_dir / "gbm"
@@ -73,34 +89,47 @@ def main(args):
     feature_train = pd.read_parquet(features_data_dir / "train" / "features")
     feature_test = pd.read_parquet(features_data_dir / "test" / "features")
 
+    if feature_processor_path.exists():
+        with open(feature_processor_path, "rb") as f:
+            feature_processor = pickle.load(f)
+    else:
+        age_scaler, gender_encoder, race_encoder = (
+            StandardScaler(),
+            OneHotEncoder(),
+            OneHotEncoder(),
+        )
+        age_scaler = age_scaler.fit(feature_train[["age_at_index"]].to_numpy())
+        gender_encoder = gender_encoder.fit(
+            feature_train[["gender_concept_id"]].to_numpy()
+        )
+        race_encoder = race_encoder.fit(feature_train[["gender_concept_id"]].to_numpy())
+        feature_processor = {
+            "age_scaler": age_scaler,
+            "gender_encoder": gender_encoder,
+            "race_encoder": race_encoder,
+        }
+        with open(feature_processor_path, "wb") as f:
+            pickle.dump(feature_processor, f)
+
     if logistic_test_result_file.exists():
         print(
             f"The results for logistic regression already exist at {logistic_test_result_file}"
         )
     else:
-        # Check if features need flattening and flatten if necessary
-        train_features = np.stack(
-            feature_train["features"].apply(lambda x: np.array(x).flatten())
-        )
-        train_labels = feature_train["boolean_value"].to_numpy()
-
-        # Similarly flatten test features
-        test_features = np.stack(
-            feature_test["features"].apply(lambda x: np.array(x).flatten())
-        )
-        test_labels = feature_test["boolean_value"].to_numpy()
+        train_dataset = prepare_dataset(feature_train, feature_processor)
+        test_dataset = prepare_dataset(feature_test, feature_processor)
         # Train logistic regression
         model = LogisticRegressionCV(scoring="roc_auc")
-        model.fit(train_features, train_labels)
-        y_pred = model.predict_log_proba(test_features)[:, 1]
+        model.fit(train_dataset["features"], train_dataset["boolean_value"])
+        y_pred = model.predict_log_proba(test_dataset["features"])[:, 1]
 
         logistic_predictions = pd.DataFrame(
             {
-                "subject_id": feature_test["subject_id"].tolist(),
-                "prediction_time": feature_test["prediction_time"].tolist(),
+                "subject_id": test_dataset["subject_id"].tolist(),
+                "prediction_time": test_dataset["prediction_time"].tolist(),
                 "predicted_boolean_probability": y_pred.tolist(),
                 "predicted_boolean_value": None,
-                "boolean_value": feature_test["boolean_value"].astype(bool).tolist(),
+                "boolean_value": test_dataset["boolean_value"].astype(bool).tolist(),
             }
         )
         logistic_test_predictions = logistic_dir / "test_predictions"
@@ -109,8 +138,10 @@ def main(args):
             logistic_test_predictions / "test_gbm_predictions.parquet"
         )
 
-        roc_auc = roc_auc_score(feature_test["boolean_value"], y_pred)
-        precision, recall, _ = precision_recall_curve(test_labels, y_pred)
+        roc_auc = roc_auc_score(test_dataset["boolean_value"], y_pred)
+        precision, recall, _ = precision_recall_curve(
+            test_dataset["boolean_value"], y_pred
+        )
         pr_auc = auc(recall, precision)
 
         metrics = {"roc_auc": roc_auc, "pr_auc": pr_auc}
@@ -123,8 +154,8 @@ def main(args):
     else:
         lightgbm_study = optuna.create_study()  # Create a new study.
         train_split, dev_split = train_test_split(feature_train, test_size=0.2)
-        train_data = prepare_gbm_dataset(train_split)
-        dev_data = prepare_gbm_dataset(dev_split)
+        train_data = prepare_dataset(train_split, feature_processor)
+        dev_data = prepare_dataset(dev_split, feature_processor)
         lightgbm_study.optimize(
             functools.partial(
                 lightgbm_objective, train_data=train_data, dev_data=dev_data
@@ -135,24 +166,22 @@ def main(args):
         best_num_trees = lightgbm_study.best_trial.user_attrs["num_trees"]
         best_params = lightgbm_study.best_trial.params
         best_params.update({"objective": "binary", "metric": "auc", "verbosity": -1})
-        full_train_set = prepare_gbm_dataset(feature_train)
+        full_train_set = prepare_dataset(feature_train)
         dtrain_final = lgb.Dataset(
             full_train_set["features"], label=full_train_set["boolean_value"]
         )
         gbm_final = lgb.train(best_params, dtrain_final, num_boost_round=best_num_trees)
 
-        test_features = np.stack(
-            feature_test["features"].apply(lambda x: np.array(x).flatten())
-        )
-        lightgbm_preds = gbm_final.predict(test_features, raw_score=False)
+        test_data = prepare_dataset(feature_test, feature_processor)
+        lightgbm_preds = gbm_final.predict(test_data["features"], raw_score=False)
 
         lightgbm_predictions = pd.DataFrame(
             {
-                "subject_id": feature_test["subject_id"].tolist(),
-                "prediction_time": feature_test["prediction_time"].tolist(),
+                "subject_id": test_data["subject_id"].tolist(),
+                "prediction_time": test_data["prediction_time"].tolist(),
                 "predicted_boolean_probability": lightgbm_preds.tolist(),
                 "predicted_boolean_value": None,
-                "boolean_value": feature_test["boolean_value"].astype(bool).tolist(),
+                "boolean_value": test_data["boolean_value"].astype(bool).tolist(),
             }
         )
         gbm_test_predictions = gbm_dir / "test_predictions"
@@ -162,10 +191,10 @@ def main(args):
         )
 
         final_lightgbm_auroc2 = -roc_auc_score(
-            feature_test["boolean_value"], lightgbm_preds
+            test_data["boolean_value"], lightgbm_preds
         )
         gbm_precision, gbm_recall, _ = precision_recall_curve(
-            feature_test["boolean_value"], lightgbm_preds
+            test_data["boolean_value"], lightgbm_preds
         )
         gbm_pr_auc = auc(gbm_recall, gbm_precision)
         final_lightgbm_auroc = lightgbm_objective(
