b9a3873b83129442acb5684376c0795d02f982f7
Chao Pang <ChaoPang229@gmail.com>
2025-04-19 20:10:29 -0400

    implemented get_eval_dataloader

--- src/cehrgpt/runners/sample_packing_trainer.py
+++ src/cehrgpt/runners/sample_packing_trainer.py
@@ -1,3 +1,5 @@
+from typing import Optional, Union
+
 from datasets import Dataset
 from torch.utils.data import DataLoader
 from transformers import Trainer
@@ -8,7 +10,7 @@ from cehrgpt.data.sample_packing_sampler import SamplePackingSampler
 
 DEFAULT_MAX_TOKENS_PER_BATCH = 16384
 
-LOG = logging.get_logger(__name__)
+LOG = logging.get_logger("transformers")
 
 
 class SamplePackingTrainer(Trainer):
@@ -33,8 +35,11 @@ class SamplePackingTrainer(Trainer):
         """Returns the training dataloader with our custom batch sampler."""
         train_dataset = self.train_dataset
 
+        LOG.info("Started computing lengths for the train dataset")
         # Calculate lengths of all sequences in dataset
         lengths = [len(sample["input_ids"]) for sample in train_dataset]
+
+        LOG.info("Finished computing lengths for the train dataset")
 
         train_dataset = self.train_dataset
         data_collator = self.data_collator
@@ -61,3 +66,75 @@ class SamplePackingTrainer(Trainer):
             "batch_sampler": batch_sampler,
         }
         return self.accelerator.prepare(DataLoader(train_dataset, **dataloader_params))
+
+    def get_eval_dataloader(
+        self, eval_dataset: Optional[Union[str, Dataset]] = None
+    ) -> DataLoader:
+        """
+        Returns the evaluation [`~torch.utils.data.DataLoader`].
+
+        Subclass and override this method if you want to inject some custom behavior.
+
+        Args:
+            eval_dataset (`str` or `torch.utils.data.Dataset`, *optional*):
+                If a `str`, will use `self.eval_dataset[eval_dataset]` as the evaluation dataset. If a `Dataset`, will override `self.eval_dataset` and must implement `__len__`. If it is a [`~datasets.Dataset`], columns not accepted by the `model.forward()` method are automatically removed.
+        """
+        if eval_dataset is None and self.eval_dataset is None:
+            raise ValueError("Trainer: evaluation requires an eval_dataset.")
+
+        # If we have persistent workers, don't do a fork bomb especially as eval datasets
+        # don't change during training
+        dataloader_key = eval_dataset if isinstance(eval_dataset, str) else "eval"
+        if (
+            hasattr(self, "_eval_dataloaders")
+            and dataloader_key in self._eval_dataloaders
+            and self.args.dataloader_persistent_workers
+        ):
+            return self.accelerator.prepare(self._eval_dataloaders[dataloader_key])
+
+        eval_dataset = (
+            self.eval_dataset[eval_dataset]
+            if isinstance(eval_dataset, str)
+            else eval_dataset if eval_dataset is not None else self.eval_dataset
+        )
+
+        LOG.info("Started computing lengths for the evaluation dataset")
+        # Calculate lengths of all sequences in dataset
+        lengths = [len(sample["input_ids"]) for sample in eval_dataset]
+        LOG.info("Finished computing lengths for the evaluation dataset")
+
+        data_collator = self.data_collator
+
+        if import_utils.is_datasets_available() and isinstance(eval_dataset, Dataset):
+            eval_dataset = self._remove_unused_columns(
+                eval_dataset, description="evaluation"
+            )
+        else:
+            data_collator = self._get_collator_with_removed_columns(
+                data_collator, description="evaluation"
+            )
+
+        # Create our custom batch sampler
+        batch_sampler = SamplePackingSampler(
+            lengths=lengths,
+            max_tokens=self.max_tokens_per_batch,
+            world_size=self.args.world_size,
+            drop_last=self.args.dataloader_drop_last,
+        )
+        dataloader_params = {
+            "collate_fn": data_collator,
+            "num_workers": self.args.dataloader_num_workers,
+            "pin_memory": self.args.dataloader_pin_memory,
+            "persistent_workers": self.args.dataloader_persistent_workers,
+            "batch_sampler": batch_sampler,
+        }
+        # accelerator.free_memory() will destroy the references, so
+        # we need to store the non-prepared version
+        eval_dataloader = DataLoader(eval_dataset, **dataloader_params)
+        if self.args.dataloader_persistent_workers:
+            if hasattr(self, "_eval_dataloaders"):
+                self._eval_dataloaders[dataloader_key] = eval_dataloader
+            else:
+                self._eval_dataloaders = {dataloader_key: eval_dataloader}
+
+        return self.accelerator.prepare(eval_dataloader)
