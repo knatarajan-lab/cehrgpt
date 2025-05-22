c72bd2d662002c5c23762aae3b5cfffdb22a89e7
Chao Pang <ChaoPang229@gmail.com>
2025-05-22 11:31:07 -0400

    added an option to add an END token in finetune_runner

--- src/cehrgpt/runners/hf_cehrgpt_finetune_runner.py
+++ src/cehrgpt/runners/hf_cehrgpt_finetune_runner.py
@@ -359,6 +359,7 @@ def main():
             SamplePackingCehrGptDataCollator,
             cehrgpt_args.max_tokens_per_batch,
             config.max_position_embeddings,
+            add_end_token_in_sample_packing=cehrgpt_args.add_end_token_in_sample_packing,
         )
     else:
         trainer_class = Trainer
