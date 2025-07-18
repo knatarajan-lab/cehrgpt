ed3d5859db909beaef3e552a721e36faec4b6b64
Chao Pang <ChaoPang229@gmail.com>
2025-07-18 11:39:36 -0400

    removed RL related code

--- src/cehrgpt/models/rl/rewards/__init__.py
+++
@@ -1,8 +0,0 @@
-from cehrgpt.models.rl.rewards.instruct_cehrgpt.covid_reward import CovidReward
-from cehrgpt.models.rl.rewards.instruct_cehrgpt.prompt_concept_reward import (
-    PromptConceptReward,
-)
-from cehrgpt.models.rl.rewards.instruct_cehrgpt.validation_patient_reward import (
-    PatientValidationReward,
-)
-from cehrgpt.models.rl.rewards.reward_model import CEHRGPTRewardModel
